814633056c940c7afa8d664087e5cf78c52f2c35
Alex Snell <alexksnell@gmail.com>
2020-05-21 09:59:29 -0400

    Update Program.cs
    Improved formatting

--- Program.cs
+++ Program.cs
@@ -4,22 +4,29 @@ namespace JurassicPark
 {
     class Program
     {
-        // Here lies jank and glue.
         static void Main(string[] args)
         {
+            Park park = new Park(); 
+            
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            Park zoo = new Park();
+            
             Logger log = new Logger(name, true);
+            DinosaurController dinoCon = new DinosaurController(name, zoo, log);
+            DinosaurView dinoView = new DinosaurView(dinoCon, log);
+            
+            // Attempt to load database.
             if (zoo.Load())
             {
                 log.Info("loaded database file");
             } else {
                 log.Info("could not load database file");
             }
-            DinosaurController dinoCon = new DinosaurController("Alex", zoo, log);
-            DinosaurView dinoView = new DinosaurView(dinoCon, log);
+            
+            // Run the view.          
             dinoView.Menu();
+            
+            // Save after Menu returns.
             zoo.Save();
         }
     }
